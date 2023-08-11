1aeec79b4b4088daee4971eca9efc130d491838c
[vrushalipachchigar] <[vrushalipachchigar@gmail.com]>
2023-08-11 11:15:17 -0400

    bug

--- src/app/_services/auth.service.ts
+++ src/app/_services/auth.service.ts
@@ -46,10 +46,5 @@ export class AuthService {
   logout(): Observable<any> {
     return this.http.post(AUTH_API + 'signout', {}, httpOptions);
   }
-
-  isAuthenticated(): Observable<boolean> {
-    // Make a request to your backend to check authentication status
-    return this.http.get<boolean>(AUTH_API + 'check-auth', httpOptions);
-  }
 }
 
--- src/app/components/add-medicine/add-medicine.component.ts
+++ src/app/components/add-medicine/add-medicine.component.ts
@@ -3,9 +3,6 @@ import { MedicineService } from 'src/app/services/medicine.service';
 import { Medicine } from 'src/app/models/medicine.model';
 import { Category } from 'src/app/models/category.model';
 import { CategoryService } from 'src/app/services/category.service';
-import { OnInit } from '@angular/core';
-import { Router } from '@angular/router'; // Import the Router module
-import { AuthService } from 'src/app/_services/auth.service';
 
 @Component({
   selector: 'app-add-medicine',
@@ -27,9 +24,7 @@ export class AddMedicineComponent {
 
   constructor(
     private medicineService: MedicineService,
-    private categoryService: CategoryService,
-    private router: Router,
-    private authService: AuthService,
+    private categoryService: CategoryService
   ) {}
 
   ngOnInit(): void {
@@ -38,67 +33,22 @@ export class AddMedicineComponent {
     });
   }
 
-  // saveMedicine(): void {
-  //   // const data = {
-  //   //   name: this.medicine.name,
-  //   //   description: this.medicine.description,
-  //   //   price: this.medicine.price,
-  //   //   quantity: this.medicine.quantity,
-  //   //   category: this.medicine.category,
-  //   // };
-  //   // this.medicineService.create(data).subscribe({
-  //   //   next: (res) => {
-  //   //     console.log(res);
-  //   //     this.submitted = true;
-  //   //   },
-  //   //   error: (e) => console.error(e),
-  //   // });
-  //   if (this.authService.isAuthenticated()) {
-  //     const data = {
-  //       name: this.medicine.name,
-  //       description: this.medicine.description,
-  //       price: this.medicine.price,
-  //       quantity: this.medicine.quantity,
-  //       category: this.medicine.category,
-  //     };
-  //     this.medicineService.create(data).subscribe({
-  //       next: (res) => {
-  //         console.log(res);
-  //         this.submitted = true;
-  //       },
-  //       error: (e) => console.error(e),
-  //     });
-  //   } else {
-  //     // User is not logged in, redirect to the login page
-  //     this.router.navigate(['/login']); // 'login' should be replaced with your actual login route
-  //   }
-  // }
-
   saveMedicine(): void {
-    // Check if the user is logged in
-    this.authService.isAuthenticated().subscribe((isAuthenticated) => {
-      if (isAuthenticated) {
-        const data = {
-          name: this.medicine.name,
-          description: this.medicine.description,
-          price: this.medicine.price,
-          quantity: this.medicine.quantity,
-          category: this.medicine.category,
-        };
-        this.medicineService.create(data).subscribe({
-          next: (res) => {
-            console.log(res);
-            this.submitted = true;
-          },
-          error: (e) => console.error(e),
-        });
-      } else {
-        // User is not logged in, redirect to the login page
-        this.router.navigate(['/login']); // 'login' should be replaced with your actual login route
-      }
+    const data = {
+      name: this.medicine.name,
+      description: this.medicine.description,
+      price: this.medicine.price,
+      quantity: this.medicine.quantity,
+      category: this.medicine.category,
+    };
+    this.medicineService.create(data).subscribe({
+      next: (res) => {
+        console.log(res);
+        this.submitted = true;
+      },
+      error: (e) => console.error(e),
     });
   }
-  
 
   newMedicine(): void {
     this.submitted = false;
